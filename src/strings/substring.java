f85149ce5797bb96729ca57aac1adcf16521723f
Bhumika awasthi <bhumikaawasthi1@gmail.com>
2025-03-19 21:36:47 +0530

    print all the substrings of a given string

--- src/strings/substring.java
+++ src/strings/substring.java
@@ -7,5 +7,12 @@ public class substring {
         System.out.println(s.substring(1,4)); //gets that part of the string from i to j-1
         System.out.println(s.substring(2,2)); //nothing will print
         System.out.println(s.substring(0,5)); //complete string will print
+    //print all substrings
+        System.out.println("all substrings are:");
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i+1; j <=5 ; j++) {
+                System.out.println(s.substring(i,j)+" ");
+            }
+        }
     }
 }
