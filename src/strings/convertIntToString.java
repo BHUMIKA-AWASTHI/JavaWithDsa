95025b5422a40fa91517de21632da280a8cb7061
Bhumika awasthi <bhumikaawasthi1@gmail.com>
2025-03-21 16:37:33 +0530

    total number of digits without using any loop

--- src/strings/convertIntToString.java
+++ src/strings/convertIntToString.java
@@ -1,14 +1,13 @@
 package strings;
-
-
 import java.util.Scanner;
-
 public class convertIntToString {
     public static void main(String[] args) {
         System.out.println("enter integer :");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String s = ""+n;
+        //String s = ""+n;
+        String s = Integer.toString(n); //built in function
         System.out.println(s);
+        System.out.println(s.length());
     }
 }
