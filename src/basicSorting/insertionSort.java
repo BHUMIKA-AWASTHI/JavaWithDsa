44f9da3290514afb73f58d983a21161089861ff1
BHUMIKA-AWASTHI <Bhumikaawasthi1@gmail.com>
2025-07-05 13:02:05 +0530

    insertion sort

---
+++ src/basicSorting/insertionSort.java
@@ -0,0 +1,32 @@
+package basicSorting;
+
+public class insertionSort {
+    public static void print(int[] arr){
+        for(int ele: arr){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+    }
+    public static void swap(int[] arr, int i, int j){
+        //swap arr[i] and arr[mindx]
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j]= temp;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10,-4,-6,90,20};
+        int n = arr.length;
+        print(arr);
+        // insertion sorting
+        for (int i = 0; i < n-1; i++) { //n-1 passes
+
+            for (int j = i; j >=1; j--) {
+                if(arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+
+                } else break;;
+            }
+        } print(arr);
+    }
+}
+
